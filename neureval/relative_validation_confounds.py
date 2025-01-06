d8945258ded35c535cd5732db15263c708a6f41a
Federica Colombo <85490656+fede-colombo@users.noreply.github.com>
2025-01-06 16:36:25 +0100

    Update relative_validation_confounds.py

--- neureval/relative_validation_confounds.py
+++ neureval/relative_validation_confounds.py
@@ -1,3 +1,9 @@
+"""
+@author: Federica Colombo
+         Psychiatry and Clinical Psychobiology Unit, Division of Neuroscience, 
+         IRCCS San Raffaele Scientific Institute, Milan, Italy
+"""
+
 import numpy as np
 from sklearn.metrics import zero_one_loss
 from neureval.utils import kuhn_munkres_algorithm
@@ -6,10 +12,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+import warnings
 
 logging.basicConfig(format='%(asctime)s, %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
                     level=logging.INFO)
+                    
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 class RelativeValidationConfounds:
@@ -28,96 +37,74 @@ class RelativeValidationConfounds:
     :type nrand: int
     """
 
-    def __init__(self, s, c, preprocessing=None, nrand=10):
+    def __init__(self, c, s, preprocessing=None, nrand=10):
         """
         Construct method.
         """
-        self.class_method = s
         self.clust_method = c
+        self.class_method = s
         self.preproc_method = preprocessing
         self.nrand = nrand
         
-    def GLMcorrection(self, X_train, Y_train, covar_train, X_test, covar_test, num_col_DTI=None):
+    def GLMcorrection_confounds(self, modalities,covariates, tr_idx, val_idx):
         """
         Eliminate the confound of covariate, such as age and sex, from the input features.
-        :param X_train: array, training features
-        :param Y_train: array, training labels
-        :param covar_train: array, training covariate data
-        :param X_test: array, test labels
-        :param covar_test: array, test covariate data
-        :param num_col_DTI: index of dataset column corresponding to white matter features (i.e., "ACR" column)
+        :param modalities: dictionary specifying the datasets separated for each type of input features
+        :type modalities: dictionary
+        :param covariates: dictionary specifying the covariates to be rgeressed out for each type of fetaure
+        :type covariates: dictionary
+        :param tr_idx: index specifying the training dataset
+        :type tr_idx: array
+        :param val_idx: index specifying the validation dataset
+        :type val_idx: array
         :return: corrected training & test feature data
         
         """
-        if num_col_DTI is not None:
-            X_train_DTI = X_train[:,num_col_DTI-3:]
-            X_test_DTI = X_test[:,num_col_DTI-3:]
-            num_col_GM = len(X_train.T) - len(X_train_DTI.T)
-            X_train_GM = X_train[:,:num_col_GM]
-            X_test_GM = X_test[:,:num_col_GM]
-            Y_array = X_train[:,1]
-            cov_train_GM = covar_train
-            cov_test_GM = covar_test
-            cov_train_DTI = covar_train[:,:len(covar_train.T)-1]
-            cov_test_DTI = covar_test[:,:len(covar_test.T)-1]
+        X_train_dic = {mod:None for mod in modalities}
+        X_test_dic = {mod:None for mod in modalities}
+        cov_train_dic = {mod:None for mod in modalities}
+        cov_test_dic = {mod:None for mod in modalities}
             
-            # normalize the covariate z-scoring
-            train_data = []
-            test_data = []
-            scaler = StandardScaler()
-            for train, test in [
-                (X_train_GM, X_test_GM),
-                (X_train_DTI, X_test_DTI),
-                (cov_train_GM, cov_test_GM),
-                (cov_train_DTI, cov_test_DTI)
-                
-            ]:
-                train = scaler.fit_transform(train)
-                test = scaler.transform(test)
-                train_data.append(train)
-                test_data.append(test)
-            
-            
-            # Adjust data for confounds of covariate
-            cor_train_data = []
-            cor_test_data = []
-            for X_tr, Y_tr, X_ts, Y_ts in [
-                (train_data[2], train_data[0], test_data[2], test_data[0]),
-                (train_data[3], train_data[1], test_data[3], test_data[1])
-            ]:
-                beta = np.linalg.lstsq(X_tr, Y_tr, rcond=None)
-                X_cor_train = (Y_tr.T - beta[0].T @ X_tr.T).T
-                X_cor_test =(Y_ts.T - beta[0].T @ X_ts.T).T
-                cor_train_data.append(X_cor_train)
-                cor_test_data.append(X_cor_test)
-            
-            X_train_cor = np.concatenate((cor_train_data[0], cor_train_data[1]), axis=1)
-            X_test_cor = np.concatenate((cor_test_data[0], cor_test_data[1]), axis=1)
+        for mod in modalities:
+            X_train_dic[mod] = np.array(modalities[mod])[tr_idx]
+            if X_train_dic[mod].ndim==1:
+                X_train_dic[mod] = X_train_dic[mod].reshape(-1,1)
+            X_test_dic[mod]= np.array(modalities[mod])[val_idx]
+            if X_test_dic[mod].ndim==1:
+                X_test_dic[mod] = X_test_dic[mod].reshape(-1,1)
+            cov_train_dic[mod] = np.array(covariates[mod])[tr_idx]
+            if cov_train_dic[mod].ndim==1:
+                cov_train_dic[mod] = cov_train_dic[mod].reshape(-1,1)
+            cov_test_dic[mod]= np.array(covariates[mod])[val_idx]
+            if cov_test_dic[mod].ndim==1:
+                cov_test_dic[mod] = cov_test_dic[mod].reshape(-1,1)
         
-        else:
-            # normalize the covariate z-scoring
-            scaler = StandardScaler()
-            train_data = []
-            test_data = []
-            for train, test in [
-                    (X_train, X_test),
-                    (covar_train, covar_test)]:
-                train = scaler.fit_transform(train)
-                test = scaler.transform(test)
-                train_data.append(train)
-                test_data.append(test)
-
-            # Adjust data for confounds of covariate
-            for X_train, Y_train, X_test, Y_test in [
-                (train_data[1], train_data[0], test_data[1], test_data[0])
-            ]:
-                beta = np.linalg.lstsq(X_train, Y_train, rcond=None)
-                X_train_cor = (Y_train.T - beta[0].T @ X_train.T).T
-                X_test_cor = (Y_test.T - beta[0].T @ X_test.T).T
+        
+        # normalize the covariate z-scoring
+        train_scaled_dic = {mod:None for mod in modalities}
+        test_scaled_dic = {mod:None for mod in modalities}
+        trcov_scaled_dic = {mod:None for mod in modalities}
+        tscov_scaled_dic = {mod:None for mod in modalities}
+        scaler = StandardScaler()
+        
+        for mod in modalities:
+            train_scaled_dic[mod] = scaler.fit_transform(X_train_dic[mod])
+            test_scaled_dic[mod] = scaler.fit_transform(X_test_dic[mod])
+            trcov_scaled_dic[mod]= scaler.fit_transform(cov_train_dic[mod])
+            tscov_scaled_dic[mod]= scaler.fit_transform(cov_test_dic[mod])
     
-        return X_train_cor, X_test_cor
+        # Adjust data for confounds of covariate
+        train_cor_dic = {mod:None for mod in modalities}
+        test_cor_dic = {mod:None for mod in modalities}
+        
+        for mod in modalities:
+            beta = np.linalg.lstsq(trcov_scaled_dic[mod], train_scaled_dic[mod], rcond=None)
+            train_cor_dic[mod] = (train_scaled_dic[mod].T - beta[0].T @ trcov_scaled_dic[mod].T).T
+            test_cor_dic[mod] = (test_scaled_dic[mod].T - beta[0].T @ tscov_scaled_dic[mod].T).T
+        
+        return train_cor_dic, test_cor_dic
 
-    def train(self, X_train_cor, tr_lab=None):
+    def train(self, train_cor_dic, tr_lab=None):
         """
         Method that performs training. It compares the clustering labels on training set
         (i.e., A(X) computed by :class:`neureval.relative_validation_confounds.RelativeValidationConfounds.clust_method`) against
@@ -126,20 +113,22 @@ class RelativeValidationConfounds:
         It returns the misclassification error, the supervised model fitted to the data,
         and both clustering and classification labels.
 
-        :param X_train_cor: counfound corrected training dataset.
-        :type X_train_cor: ndarray, (n_samples, n_features)
+        :param train_cor_dic: dictionary including the confound corrected training datasets for each modality
+        :type train_cor_dic: dictionary
         :param tr_lab: cluster labels found during CV for clustering methods with no `n_clusters` parameter.
             If not None the clustering method is not performed on the whole test set. Default None.
         :type tr_lab: list
         :return: misclassification error, fitted supervised model object, clustering and classification labels.
         :rtype: float, object, ndarray (n_samples,)
         """
+        X_train_cor = np.concatenate([np.array(train_cor_dic[mod]) for mod in train_cor_dic], axis=1)
+        
         if self.preproc_method is not None:
             fitpreproc_tr = self.preproc_method.fit(X_train_cor)
             X_train_cor = fitpreproc_tr.transform(X_train_cor)
         else:
             X_train_cor = X_train_cor
-            
+        
         if tr_lab is None:
             clustlab_tr = self.clust_method.fit_predict(X_train_cor)  # A_k(X)
         else:
@@ -152,12 +141,10 @@ class RelativeValidationConfounds:
         classlab_tr = fitclass_tr.predict(X_train_cor)
         misclass = zero_one_loss(clustlab_tr, classlab_tr)
         
-        if self.preproc_method is not None:
-            return misclass, fitclass_tr, clustlab_tr, fitpreproc_tr
-        else:
-            return misclass, fitclass_tr, clustlab_tr
+        return misclass, fitclass_tr, clustlab_tr, X_train_cor
+        
 
-    def test(self, X_test_cor, fit_model, fit_preproc=None):
+    def test(self, test_cor_dic, fit_model, fit_preproc=None):
         """
         Method that compares test set clustering labels (i.e., A(X'), computed by
         :class:`neureval.relative_validation_confounds.RelativeValidationConfounds.clust_method`) against
@@ -167,8 +154,8 @@ class RelativeValidationConfounds:
         It returns the misclassification error, together with
         both clustering and classification labels.
 
-        :param X_test_cor: confound corrected test dataset.
-        :type X_test_cor: ndarray, (n_samples, n_features)
+        :param test_cor_dic: dictionary including the confound corrected test datasets for each modality
+        :type test_cor_dic: dictionary
         :param fit_model: fitted supervised model.
         :type fit_model: class
         :param fit_preproc: fitted preprocessing method.
@@ -176,8 +163,10 @@ class RelativeValidationConfounds:
         :return: misclassification error, clustering and classification labels.
         :rtype: float, dictionary of ndarrays (n_samples,)
         """
+        X_test_cor = np.concatenate([np.array(test_cor_dic[mod]) for mod in test_cor_dic], axis=1)
+        
         if fit_preproc is not None:
-            X_test_cor = fit_preproc.transform(X_test_cor)
+            X_test_cor = self.preproc_method.transform(X_test_cor)
         else:
             X_test_cor = X_test_cor
         
@@ -185,11 +174,12 @@ class RelativeValidationConfounds:
         if len([cl for cl in clustlab_ts if cl >= 0]) == 0:
             logging.info(f"No clusters found during testing with {self.clust_method}")
             return None
+        
         classlab_ts = fit_model.predict(X_test_cor)
         bestperm = kuhn_munkres_algorithm(np.int32(classlab_ts), np.int32(clustlab_ts))  # array of integers
         misclass = zero_one_loss(classlab_ts, bestperm)
         
-        return misclass, bestperm
+        return misclass, bestperm, X_test_cor
 
     def rndlabels_traineval(self, X_train_cor, X_test_cor, train_labels, test_labels):
         """
@@ -235,3 +225,4 @@ class RelativeValidationConfounds:
         pred_lab = self.class_method.predict(xts)
         me_ts = zero_one_loss(pred_lab, kuhn_munkres_algorithm(np.int32(pred_lab), np.int32(labts)))
         return me_ts
+
